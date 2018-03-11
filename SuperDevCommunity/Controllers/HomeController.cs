ac4ab7d65199bb473dbe6edadaae031e9630dde0
GoncaloRod <rodriguesgonalo604@gmail.com>
2018-03-11 18:03:06 +0000

    Implemented Bootstrap 4

--- SuperDevCommunity/Controllers/HomeController.cs
+++ SuperDevCommunity/Controllers/HomeController.cs
@@ -10,19 +10,7 @@ namespace SuperDevCommunity.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Home";
 
             return View();
         }
