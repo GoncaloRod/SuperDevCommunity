665dc960e96c83ea6f7a189faf80934b1a9535ee
GoncaloRod <rodriguesgonalo604@gmail.com>
2018-04-02 15:02:07 +0100

    Made other users profile page

---
+++ SuperDevCommunity/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SuperDevCommunity.Models;
+
+namespace SuperDevCommunity.Controllers
+{
+    public class UsersController : Controller
+    {
+        private SuperDevCommunityContext db = new SuperDevCommunityContext();
+
+        public ActionResult Profile(int? id)
+        {
+            if (User.Identity.IsAuthenticated) if (id == int.Parse(User.Identity.Name)) return Redirect("/profile");
+
+            User user = db.Users.Find(id);
+
+            if (user == null) return HttpNotFound();
+
+            ViewBag.posts = db.Posts.Where(p => p.userId == user.id).ToList();
+            ViewBag.comments = db.Comments.Where(c => c.userId == user.id).ToList();
+            ViewBag.likes = new List<Post>();
+
+            foreach (var like in db.PostLikes.Where(l => l.userId == user.id))
+            {
+                ViewBag.likes.Add(db.Posts.Where(p => p.id == like.postId).ToList()[0]);
+            }
+
+            return View(user);
+        }
+    }
+}
